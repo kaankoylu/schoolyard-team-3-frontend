d19f9f01d891b8bf3142f0b35ff4bcece4efff7f
Linus <129332073+Spinatlasagnyey@users.noreply.github.com>
2025-12-09 23:54:57 +0100

    Added BG image for Student Dashboard, standard Profile picture and an improved Button that leads to the Designing page for the students

--- src/routes/dashboard/student/+page.svelte
+++ src/routes/dashboard/student/+page.svelte
@@ -1,18 +1,28 @@
 <script>
     import DashboardNavigationCard from "$lib/components/DashboardNavigationCard.svelte";
     import ProfilePicture from "$lib/components/ProfilePicture.svelte";
+    import DesigningIcon from "$lib/components/DesigningIcon.svelte";
 	import RegularButton from "$lib/components/RegularButton.svelte";
 </script>
-<div class="bg-green-200">
+<img src="/dashboard/playcarpet.jpg" 
+         alt="Play Carpet" 
+         class="absolute inset-0 w-full h-full object-cover opacity-20 pointer-events-none z-0 mt-16" />
+<div class="bg-green-400 z-10 rounded-lg">
     <div class="w-full flex p-6">
-            <div class="w-full flex flex-col items-center gap-4">
-                <ProfilePicture></ProfilePicture>
+        <div class="w-full flex flex-col items-center gap-4">
+            <ProfilePicture></ProfilePicture>
 
-                <h2 class="text-xl font-semibold">Marilin Monroe</h2>
-            </div>
-            <div class="w-full flex flex-col items-center gap-4">
-                <RegularButton name="Start Designing" href="/dashboard/student/design"></RegularButton>
-            </div>
+            <h2 class="text-xl font-semibold">Marilin Monroe</h2>
+        </div>
+        <div class="w-full flex flex-col items-center gap-4">
+            <a href="/next-page" aria-label="Start Designing" class="relative block w-60 h-40 md:w-90 md:h-56 rounded-4xl overflow-hidden shadow-lg focus:outline-none focus:ring-4 focus:ring-sky-300 group transform transition hover:scale-105 active:scale-95">
+                <DesigningIcon />
+                <div class="absolute bottom-2 w-full text-center">
+                    <span class="text-white text-md md:text-base font-semibold drop-shadow-md">Start Designing</span>
+                </div>
+
+            </a>
+        </div>
     </div>
     <div class="w-full grid grid-cols-2 gird-columns-2 p-6 gap-6">
         <DashboardNavigationCard 
